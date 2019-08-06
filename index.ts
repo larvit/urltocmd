c4334e0c0e174a26f8346d8deb4fb6d0ed333496
Lilleman <lilleman@zmartbag.com>
2019-08-06 19:57:00 +0200

    Updated source to be configurable for urls and commands

--- index.ts
+++ index.ts
@@ -1,18 +1,27 @@
 import http from 'http';
+import fs from 'fs';
 import { exec } from 'child_process';
 const port = process.env.PORT || 3000;
+const configFileName = process.env.URLTOCMD_PATH || './urltocmd.json';
 
 interface urltocmd {
 	url: string;
 	cmd: string;
 }
 
-const urltocmds: urltocmd[] = [
-	{
-		url: '/foo',
-		cmd: 'echo "tomte"'
-	}
-]
+if (!fs.existsSync(configFileName)) {
+	console.error('Config file does not exist (change with ENV URLTOCMD_PATH): "' + configFileName + '"');
+	process.exit(1);
+}
+
+const urltocmds: urltocmd[] = JSON.parse(fs.readFileSync(configFileName).toString());
+
+//const urltocmds: urltocmd[] = [
+//	{
+//		url: '/foo',
+//		cmd: 'echo "tomte"'
+//	}
+//]
 
 const urls = urltocmds.map(el => el.url);
 
