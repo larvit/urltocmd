1737612286ca31dcb0613e2b0b15109fa3458283
Lilleman <lilleman@zmartbag.com>
2019-08-07 13:19:23 +0200

    Added functionality for checking when a job is done

--- index.ts
+++ index.ts
@@ -1,76 +1,106 @@
 import * as dotenv from 'dotenv';
+import uuid from 'uuid';
 import http from 'http';
 import { exec } from 'child_process';
 
-dotenv.config();
+dotenv.config(); // Populate process.env from .env file
 
 const stdin = process.stdin;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Default to port 3000 if none is provided in process.env.PORT
 
 interface urltocmd {
 	url: string;
 	cmd: string;
 }
 
-const inputChunks: string[] = [];
-let inputStarted: boolean = false;
+const activeJobs: string[] = []; // A list of uuids of active jobs
+const inputChunks: string[] = []; // stdin will stream in chunks, this is an array where we collect them
+let inputStarted: boolean = false; // This is an indicator if we have started to recieve stdin
 
-stdin.resume();
+stdin.resume(); // Make sure we start processing stdin
 stdin.setEncoding('utf8');
 
+// We get data on stdin \o/
 stdin.on('data', chunk => {
 	inputStarted = true;
 	inputChunks.push(chunk);
 });
 
+// When all stdin is read, do this
 stdin.on('end', () => {
 	const urltocmds: urltocmd[] = JSON.parse(inputChunks.join());
 
+	// Get all valid urls
 	const urls = urltocmds.map(el => el.url);
 
+	// This functionis ran on each request to the server
 	function requestHandler(req: http.IncomingMessage, res: http.ServerResponse): void {
+
+		// req.url should always be set, but in the typescript definitions it says it can be undefined... I wonder how
 		if (req.url === undefined) {
 			res.statusCode = 400;
 			res.end('Your request is utterly invalid in a deeply disturbing way');
 			return;
 		}
 
+		if (req.url.startsWith('/status?')) {
+			const statusUuid: string = req.url.split('?')[1];
+			if (activeJobs.includes(statusUuid)) {
+				res.statusCode = 202;
+				res.end('');
+			} else {
+				res.statusCode = 404;
+				res.end('Not Found - Job might be finnished or was never created');
+			}
+			return;
+		}
+
+		// If this specific url does not exist in the stdin config, its a 404
 		if (!urls.includes(req.url)) {
 			res.statusCode = 404;
 			res.end('Not Found - No command found for URL: "' + req.url + '"');
 			return;
 		}
 
-		const cmd = urltocmds.map(el => {
-			if (el.url === req.url) {
-				return el.cmd;
-			} else {
-				return false;
-			}
-		})[0];
+		// Get the specific command for this url
+		let cmd: string | boolean = false;
+		urltocmds.forEach(el => {
+			if (el.url === req.url) cmd = el.cmd;
+		});
 
-		if (cmd === false || !cmd) {
+		if (cmd === false) {
 			res.statusCode = 500;
-			res.end('Internal Server Error - Something is wrong with the command for URL: "' + req.url + '"');
+			res.end('Internal Server Error - Command not found for URL: "' + req.url + '"');
 			return;
 		}
 
-		console.log('Running command: "' + cmd + '" for URL: "' + req.url + '"');
+		if (!cmd) {
+			res.statusCode = 500;
+			res.end('Internal Server Error - Cmd: "' + cmd + '" is invalid for URL: "' + req.url + '"');
+			return;
+		}
+
+		const jobUuid = uuid.v1();
+		activeJobs.push(jobUuid);
+
+		console.log('Running command: "' + cmd + '" for URL: "' + req.url + '" with uuid: ' + jobUuid);
 		res.statusCode = 202;
-		res.end('Accepted - Running command for URL: "' + req.url + '"');
+		res.end('Accepted - Running command for URL: "' + req.url + '"\nuuid: ' + jobUuid);
 
 		exec(cmd, (err, stdout, stderr) => {
 			if (err) {
-				console.error('Command failed: "' + cmd + '", err: ' + err.message);
+				console.error(jobUuid + ': Command failed: "' + cmd + '", err: ' + err.message);
 			}
 
 			if (stderr) {
-				console.error('stderr: ' + stderr);
+				console.error(jobUuid + ': stderr: ' + stderr);
 			}
 
 			if (stdout) {
-				console.log('stdout: ' + stdout);
+				console.log(jobUuid + ': stdout: ' + stdout);
 			}
+
+			activeJobs.splice(activeJobs.indexOf(jobUuid), 1);
 		});
 	}
 
